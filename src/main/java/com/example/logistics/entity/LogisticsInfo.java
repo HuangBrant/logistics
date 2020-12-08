97e732856d8f61ce43119edafadee1e42f9ba477
huangjiaji <15875172612@139.com>
2020-12-08 17:10:32 +0800

    项目创建

---
+++ src/main/java/com/example/logistics/entity/LogisticsInfo.java
@@ -0,0 +1,27 @@
+package com.example.logistics.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "logistics_info")
+public class LogisticsInfo {
+
+    @Id
+    private String aid;
+
+    private Integer cid;
+
+    private Integer uid;
+
+    private Date startTime;
+
+    private Date endTime;
+
+    private Integer status;
+}
